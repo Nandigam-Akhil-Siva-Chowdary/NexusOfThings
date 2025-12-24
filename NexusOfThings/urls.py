70a9cb367000ebd973b425d80232b8566437e3a0
Nandigam-Akhil-Siva-Chowdary <akhilsivachowday11@gmail.com.com>
2025-12-24 16:42:02 +0530

    initial commit

--- NexusOfThings/urls.py
+++ NexusOfThings/urls.py
@@ -3,19 +3,15 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from events import views
 
 urlpatterns = [
     path('admin-2310/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('get-event-details/<str:event_name>/', views.get_event_details, name='get_event_details'),
-    path('register-participant/', views.register_participant, name='register_participant'),
-    path('api/participants/', views.get_participants, name='get_participants'),
-    path('api/events/', views.get_events, name='get_events'),
+
+    # Delegate everything else to the events app
     path('', include('events.urls')),
 ]
 
-# Serve static files during development
+# Serve static & media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- events/urls.py
+++ events/urls.py
@@ -2,13 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Home page
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('events/', views.events_page, name='events'),
-    path('contact/', views.contact, name='contact'),
+
+    # Event data (AJAX)
     path('get-event-details/<str:event_name>/', views.get_event_details, name='get_event_details'),
+
+    # Registration
     path('register-participant/', views.register_participant, name='register_participant'),
+
+    # APIs
     path('api/participants/', views.get_participants, name='get_participants'),
     path('api/events/', views.get_events, name='get_events'),
-    path("registration/<str:team_code>/",views.registration_details,name="registration_details"),
+
+    # Registration confirmation page
+    path('registration/<str:team_code>/', views.registration_details, name='registration_details'),
 ]
